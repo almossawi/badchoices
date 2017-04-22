f0b909476d2e48685029ff20ba3975b1a7759e76
almossawi <almossawi@gmail.com>
2017-04-21 21:19:29 -0700

    Clean up endorsements, on mobile

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   var args = {};
   args.width = $(window).width();
 
+  resize();
   update_caption();
 
   $('img.cover').ready(function() {
@@ -36,6 +37,26 @@
       d3.select('.caption').html('The UK and Commonwealth edition (left) and the North American edition (right)');
     }
   }
+
+  function resize() {
+    if(window.innerWidth < 488) {
+      d3.select('p.endorsement-quote')
+        .html("<span class='serif'>&ldquo;</span>Perfect for anyone wanting to understand the basics of&nbsp;Computer&nbsp;Science.<span class='serif'>&rdquo;</span> <span class='author-inline'>&mdash;Cesar Hidalgo, Director, Collective Learning group, MIT&nbsp;Media&nbsp;Lab</span>");
+
+      d3.select('p.endorsement-author')
+        .html('');
+    } else {
+      d3.select('p.endorsement-quote')
+        .html("<span class='serif'>&ldquo;</span>Perfect for anyone wanting to understand the basics of&nbsp;Computer&nbsp;Science.<span class='serif'>&rdquo;</span>");
+
+      d3.select('p.endorsement-author')
+        .html('&mdash;Cesar Hidalgo, Director of the Collective Learning group at&nbsp;the&nbsp;MIT&nbsp;Media&nbsp;Lab');
+    }
+  }
+
+  window.addEventListener('optimizedResize', function() {
+    resize();
+  });
 
   // from mdn / resize
   var throttle = function(type, name, obj) {
